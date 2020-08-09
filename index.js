1472f7ee15aab4e502c3d804b9a6012bf17eaec0
JAVED ALI <30997178+MJavedAli@users.noreply.github.com>
2020-08-09 10:39:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+/* env-Params is a library built to make development more feasible by allowing 
+ * multiple .env files with custpmised path
+ * @Author: Javed Ali <imjavedali@gmail.com>
+ * This depends on the popular dotenv library
+ * 
+ */
+
+// Export the main library
+module.exports = require('./model/main')
